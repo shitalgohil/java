f705f61df48f0b5373ceb5fc9ce8ccd29a9ddff3
Shital Gohil <155747453+shitalgohil@users.noreply.github.com>
2024-10-18 14:52:33 +0530

    Ternary operator

---
+++ TernaryOperator.java
@@ -0,0 +1,13 @@
+
+public class TernaryOperator{
+	public static void main(String args[]){
+		int a = 10;
+		int b;
+		b= (a == 1) ? 20:30;
+		System.out.println("value of b is:"+b);
+		
+		b=(a==10)?20:30;
+		System.out.println("value of b is :"+b);
+	}
+}
+		
